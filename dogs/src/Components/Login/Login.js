b6ab455145af71a4200ee5b3fdfa97fd25bc19a1
Rafa <rafadevjs@gmail.com>
2022-06-22 17:26:45 -0300

    feat(global): Create login functionality

--- dogs/src/Components/Login/Login.js
+++ dogs/src/Components/Login/Login.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import LoginForm from './LoginForm'
+import LostPassword from "./LostPassword";
+import LoginCreate from "./LoginCreate";
+import Reset from "./Reset";
 
 const Login = () => {
   return (
     <div>
-        Login
+        <Routes >
+          <Route path="/" element={<LoginForm />}/>
+          <Route path="criar" element={<LoginCreate />}/>
+          <Route path="perdeu" element={<LostPassword />}/>
+          <Route path="resetar" element={<Reset />}/>
+        </Routes>
     </div>
   )
 }
---
+++ dogs/src/Components/Login/Reset.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const reset = () => {
+  return (
+    <div>
+      Resetar a senha
+    </div>
+  )
+}
+
+export default reset
