7326b0f9de235275cd0fe50169db471354744105
Rafa <rafadevjs@gmail.com>
2022-09-23 20:10:00 -0300

    chore(global): Change the folder dogs to root

---
+++ src/Components/Help/Image.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from "./Image.module.css";
+
+const Image = () => {
+  return (
+    <div className={styles.wrapprer}>
+      
+    </div>
+  )
+}
+
+export default Image
